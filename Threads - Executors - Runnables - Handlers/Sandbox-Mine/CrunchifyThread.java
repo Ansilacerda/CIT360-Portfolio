64e8b6300dcaefd68f07001399b4972103891591
Dan Hively <dan@hively.io>
2016-07-12 16:56:29 -0600

    More Portfolio

---
+++ Threads - Executors - Runnables - Handlers/Sandbox-Mine/CrunchifyThread.java
@@ -0,0 +1,31 @@
+package com.crunchify;
+ 
+public class CrunchifyThread {
+ 
+	/**
+	 * @author Crunchify.com
+	 */
+ 
+	public static void main(String args[]) {
+		new ThreadTest("eBay").start();
+		new ThreadTest("Paypal").start();
+		new ThreadTest("Google").start();
+	}
+}
+ 
+class ThreadTest extends Thread {
+	public ThreadTest(String str) {
+		super(str);
+	}
+ 
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Loop " + i + ": " + getName());
+			try {
+				sleep((int) (Math.random() * 2000));
+			} catch (InterruptedException e) {
+			}
+		}
+		System.out.println("Test Finished for: " + getName());
+	}
+}
